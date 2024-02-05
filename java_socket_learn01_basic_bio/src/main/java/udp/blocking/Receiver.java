dd7347f92290d0a428ad097e2de934a41f301ae5
peichao.dong <peichao.dong@thoughtworks.com>
2024-02-05 11:28:25 +0800

    add udp blocking example

---
+++ java_socket_learn01_basic_bio/src/main/java/udp/blocking/Receiver.java
@@ -0,0 +1,41 @@
+package udp.blocking;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+@Slf4j
+public class Receiver {
+    private final int port;
+    private boolean running = false;
+
+    public Receiver(int port) {
+        this.port = port;
+    }
+
+    public void start() {
+        running = true;
+        try (DatagramSocket datagramSocket = new DatagramSocket(port)) {
+            while (running) {
+                byte[] buffer = new byte[1024];
+
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                datagramSocket.receive(packet);
+                log.info("received: {}", new String(buffer, 0, packet.getLength()));
+            }
+        } catch (IOException e) {
+            log.error("error", e);
+        }
+    }
+
+    public void stop() {
+        this.running = false;
+    }
+
+    public static void main(String[] args) {
+        Receiver server = new Receiver(5454);
+        server.start();
+    }
+}
---
+++ java_socket_learn01_basic_bio/src/main/java/udp/blocking/Sender.java
@@ -0,0 +1,17 @@
+package udp.blocking;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Sender {
+    public static void main(String[] args) {
+        try (java.net.DatagramSocket socket = new java.net.DatagramSocket()) {
+            byte[] buffer = "hello server".getBytes();
+            java.net.DatagramPacket packet = new java.net.DatagramPacket(buffer, buffer.length, java.net.InetAddress.getByName("localhost"), 5454);
+            socket.send(packet);
+        } catch (java.io.IOException e) {
+            log.error("error", e);
+        }
+    }
+}
